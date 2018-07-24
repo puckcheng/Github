e7e89e401e850b9bdabeed397f1ff7050e9cf7cd
puckcheng <puckcheng@gmail.com>
2018-07-23 22:24:55 -0600

    update

--- Transcoding/Convert bat/archive.bat
+++ Transcoding/Convert bat/archive.bat
@@ -10,20 +10,29 @@ rem static end
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 SETLOCAL
+
+set /P list="Generate list.txt?  "
+if "%list%"=="y" goto ALL 
+if "%list%"=="n" goto PART
+goto EOF
+
+:ALL
 dir /b > list.tmp
 type list.tmp | findstr /e .MP4 >> list.txt
 del list.tmp
 
+:PART
 FOR /F %%g in (list.txt) DO (
 set name=%%g
+set name=!name:~0,8!
 
-%mediainfo% "--Inform=Video;%%FrameCount%%" "%~dp1!name!" > "D:\Media\TEMP\frames.txt"
+%mediainfo% "--Inform=Video;%%FrameCount%%" "%~dp1!name!.MP4" > "D:\Media\TEMP\frames.txt"
 
 echo import vapoursynth as vs > "!name!.vpy"
 echo import sys >> "!name!.vpy"
 echo core=vs.get_core(accept_lowercase=True,threads=4^) >> "!name!.vpy"
 echo core.max_cache_size=8000 >> "!name!.vpy"
-echo source=r'%~dp1!name!' >> "!name!.vpy"
+echo source=r'%~dp1!name!.MP4' >> "!name!.vpy"
 echo src=core.lsmas.LWLibavSource(source,threads=1^) >> "!name!.vpy"
 echo src=core.std.SetFrameProp(src,prop="_FieldBased",intval=0^) >> "!name!.vpy"
 echo src.set_output(^) >> "!name!.vpy"
@@ -33,10 +42,10 @@ call :DoEncode !name!
 ENDLOCAL
 
 Echo  
-::ping 127.0.0.1 -n 1 >nul
-::mshta vbscript:createobject("sapi.spvoice").speak("Job done!")(window.close)
-Echo done!
-goto :EOF
+ping 127.0.0.1 -n 1 >nul
+mshta vbscript:createobject("sapi.spvoice").speak("Job done!")(window.close)
+Echo done^^!
+goto EOF
 
 :DoEncode
 
@@ -48,23 +57,24 @@ FOR /F %%i in (D:\Media\TEMP\frames.txt) DO set tframes=%%i
 --y4m --preset slow --frame-threads 8 ^
 --frames %tframes% --output-depth 10 --crf 18 ^
 --qcomp 0.65 --merange 44 --aq-strength 0.8 ^
---output "D:\Media\TEMP\%~1_tmp.mp4" -
+--range full --colorprim bt709 --transfer bt709 --colormatrix bt709 ^
+--output "D:\Media\TEMP\%~1.tmp" -
 
 ::ping 127.0.0.1 -n 3 >nul
 
-%ffmpeg% -fflags +genpts -i "D:\Media\TEMP\%~1_tmp.mp4" -i "%~1" -map 0:v -map 1:a -vcodec copy  -acodec copy "%~1_arc.mp4"
+%ffmpeg% -fflags +genpts -i "D:\Media\TEMP\%~1.tmp" -i "%~1.MP4" -map 0:v -map 1:a -vcodec copy  -acodec copy "%~1_arc.mp4"
 
 ::ping 127.0.0.1 -n 1 >nul
 
 del "%~1.vpy"
-del "%~1.lwi"
+del "%~1.MP4.lwi"
 del "D:\Media\TEMP\frames.txt"
-del "D:\Media\TEMP\%~1_tmp.mp4"
+del "D:\Media\TEMP\%~1.tmp"
 cls
 
 EXIT /B
 
 :EOF
-
+pause
 
 
